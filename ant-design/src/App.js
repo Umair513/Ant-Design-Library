251561dc0af19b65ec477a3c99a61f518306ef6b
Umair513 <soomroumair513@gmail.com>
2023-06-11 12:07:35 +0500

    Alert and message Components

--- ant-design/src/App.js
+++ ant-design/src/App.js
@@ -21,7 +21,7 @@ function App() {
   }
   return ( 
     <div className="App">
-     <AntTable></AntTable>
+     <AntForm></AntForm>
     </div>
   );
 }
--- ant-design/src/components/AntForm.js
+++ ant-design/src/components/AntForm.js
@@ -1,13 +1,23 @@
-import { Button, Form, Input } from 'antd'
-import React from 'react'
+import { Alert, Button, Form, Input, message } from 'antd'
+import React, { useState } from 'react'
 
 const AntForm = () => {
+    const [showAlert, setShowAlert] = useState(false)
     const onFinish = (e) => {
         console.log(e)
+        setTimeout(() => {
+            // message.success("Login Successful")
+            // message.error("Login Failed")
+            setShowAlert(true)
+            // message.warning("Warning")
+        },2000)
     }
     return (
         <div className='App'>
             <div className='App-header'>
+                {
+                    showAlert && <Alert type='error' message="Error" description="There was an error on login" closable></Alert>
+                }
                 <Form onFinish={onFinish}>
                     <Form.Item label="User Name" name="username" >
                         <Input placeholder='User name' required></Input>
